dae275effdeb915610ae69b1484890214794e102
darksec <37667454+r3x07@users.noreply.github.com>
2020-05-14 01:38:28 +0500

    Updated install.sh
    Edits: added git cloning from original repo of network drivers

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt-get install build-essentials git dkms
 sudo dpkg -i *.deb
 uname -sr
+git clone https://github.com/tomaspinho/rtl8821ce
 cd rtl8821ce
 chmod +x dkms-install.sh
 chmod +x dkms-remove.sh
